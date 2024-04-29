d127c35d502f39ae5275c954a1c42aec07d53dc2
hailer-MIT <hailomasegede@gmail.com>
2024-04-29 00:52:24 -0700

    latestupdates

--- src/components/MainHeader/MainHeader.js
+++ src/components/MainHeader/MainHeader.js
@@ -6,7 +6,7 @@ import classes from './MainHeader.module.css';
 const MainHeader = (props) => {
   return (
     <header className={classes['main-header']}>
-      <h1>A Typical Page</h1>
+      <h3>Hailer <i style={{fontSize : '26px' }}> <b> login + validation </b> </i> Page using Reactjs</h3>
       <Navigation isLoggedIn={props.isAuthenticated} onLogout={props.onLogout} />
     </header>
   );
